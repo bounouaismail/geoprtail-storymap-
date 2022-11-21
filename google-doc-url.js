c8561075dcf3d31227f6a128de3ff83b601d7056
bounouaismail <107546720+bounouaismail@users.noreply.github.com>
2022-11-21 12:04:23 +0100

    Update google-doc-url.js
    sheet des infos

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AAwVm8T3v737acJUqpk9PlCOlKYLXXrLmkeFutuiiFA/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyABiqQ9jJoZI2wy6xdt7Wngx7Vr65ndPjg';
